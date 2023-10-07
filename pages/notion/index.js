3d24f15faa016d144a6964fc50208e90e5d78017
elpapi42 <whitman-2@hotmail.com>
2023-10-07 18:22:02 -0500

    Add redirect page

---
+++ pages/notion/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Navbar from "../../components/navbar";
+import Privacy from "../../components/privacy";
+
+const Notion = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Notion;
